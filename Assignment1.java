719db550043b0c20ca3b778afc6f7d007c98b2d4
HarshaPrada <harshu07@gmail.com>
2017-04-03 15:07:06 +0530

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+package corejavaassignment;
+
+public class Assignment1
+{	  
+    public static void main(String args[]) 
+    {  
+       System.out.println(" Sum of 110 add 200 is : " + add(110, 200));
+       System.out.println(" Sum of 0 and 0 is : " + add(0, 0));
+       System.out.println(" Sum of -10 and +10 is : " + add(-10, 10));
+       System.out.println(" Sum of -10 + 200 is : " + add(-10, 200));
+       System.out.println(" Sum of 0 + 200 is : " + add(0, 200));
+    }  
+  
+    public static int add(int a, int b)
+    {
+        if(b == 0) 
+        return a;
+        int sum = a ^ b; 
+        int carry = (a & b) << 1;
+        return add(sum, carry);
+    }
+}
